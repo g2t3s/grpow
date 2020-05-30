4c361d5eae4fb76ebceca23448526fa5c84184bf
Gitardo <gerardo.tejada.saracho@gmail.com>
2020-05-30 18:04:31 +0200

    register, sign in and sign out - ready

--- lib/grpow_web/router.ex
+++ lib/grpow_web/router.ex
@@ -1,5 +1,6 @@
 defmodule GrpowWeb.Router do
   use GrpowWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -11,6 +12,12 @@ defmodule GrpowWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
   end
 
   scope "/", GrpowWeb do
